3a4fc49efb1707672d7de00b2cef39f1827b4e0c
alec-cov <80437740+alec-cov@users.noreply.github.com>
2022-04-09 23:50:25 -0500

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,5 @@
 export default function Card(props){
-let {name, username} = props.persona;
+let {name, username, phone, website, email} = props.persona;
 console.log(props.persona)
 
     return(
@@ -8,8 +8,10 @@ console.log(props.persona)
   <div className="card">
   <div className="card-body">
     <h5 className="card-title">{name}</h5>
-    <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <h8 className="username">{username}</h8>
+    <h6 className="phone">{phone}</h6>
+    <h6 className="email">{email}</h6>
+    <p className="website">{website}</p>
   </div>
 </div>
 
